53168341938ce5a1f6913889f5a8ee51ef5e9ca4
Emilio Larrambebere <emiliolarrambebere@gmail.com>
2016-11-15 16:49:01 +0000

    Added additional test for string reversal
     - Double reversing string should return the original string
     - It should handle Unicode characters

--- CodingTest/UnitTests/ReversingString/StringUtiltiesTests.cs
+++ CodingTest/UnitTests/ReversingString/StringUtiltiesTests.cs
@@ -17,5 +17,30 @@ namespace UnitTests.ReversingString
 
 			Assert.AreEqual(expectedResult, result);
 		}
+
+        [Test]
+        public void ReverseUnicodeStringShouldReturnTheStringInTheReverseOrder()
+        {
+            const string input = "Räksmörgås";
+            const string expected = "sågrömskäR";
+
+            StringUtilities stringUtilities = new StringUtilities();
+            string result = stringUtilities.Reverse(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase("orangebus")]
+        [TestCase("skeleton")]
+        [TestCase("OrangeBus")]
+        [TestCase("Räksmörgås")]
+        public void DoubleReverseStringShouldReturnTheOriginalString(string input)
+        {
+            StringUtilities stringUtilities = new StringUtilities();
+
+            string result = stringUtilities.Reverse(stringUtilities.Reverse(input));
+
+            Assert.AreEqual(input, result);
+        }
 	}
 }
